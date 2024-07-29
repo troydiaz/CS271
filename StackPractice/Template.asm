0f3e6d55dc627ef7873dd0eff58c63e840e1b28d
Troy Diaz <86332243+troydiaz@users.noreply.github.com>
2024-07-28 18:51:32 -1000

    Add files via upload

---
+++ StackPractice/Template.asm
@@ -0,0 +1,40 @@
+TITLE Program Template     (template.asm)
+
+; Author: 
+; Last Modified:
+; OSU email address: ONID_ID@oregonstate.edu
+; Course number/section:   CS271 Section ???
+; Project Number:                 Due Date:
+; Description: This file is provided as a template from which you may work
+;              when developing assembly projects in CS271.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	push 1
+	push 2
+	push -1
+	push -2
+	pop EAX
+	pop EAX
+	pop EAX
+	pop EAX
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
